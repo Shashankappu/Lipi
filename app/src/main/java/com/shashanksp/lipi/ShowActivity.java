a050dc3a953f2d7ee8bf5e06209499ab5cbadb9f
Shashankappu <Shashankprince523@gmail.com>
2023-04-29 23:46:16 +0530

    Languages spinner added and background for show activity changed

--- app/src/main/java/com/shashanksp/lipi/ShowActivity.java
+++ app/src/main/java/com/shashanksp/lipi/ShowActivity.java
@@ -3,12 +3,28 @@ package com.shashanksp.lipi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Toast;
 
 public class ShowActivity extends AppCompatActivity {
-
+    AutoCompleteTextView lang_tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show);
+        lang_tv = findViewById(R.id.lang_btn);
+        String[] lang  = new String[]{"En","Kan","Hin","Ta","Te"};
+        ArrayAdapter<String> lang_adapter = new ArrayAdapter<>(this,R.layout.customdropdown,lang);
+        lang_tv.setAdapter(lang_adapter);
+
+        lang_tv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(ShowActivity.this,"Language changed to "+lang_tv.getText(),Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
